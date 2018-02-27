e047c3e7831b6ec88343877e58fb9f5f3fcf7272
scottHargus <scotteh@mydigitalhome.net>
2018-02-26 22:07:11 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/* header( “HTTP/1.1 301 Moved Permanently” ); */
+header("Location: https://illinoisfirejuniors.sharepoint.com/sites/group.operations/SitePages/Home.aspx"); /* Redirect browser */
+
+/* Make sure that code below does not get executed when we redirect. */
+exit;
+?>
