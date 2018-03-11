d8b825589af8c6bab953c763a701384b624d57e5
scottHargus <scotteh@mydigitalhome.net>
2018-03-11 10:58:53 -0500

    test

--- redirect.php
+++ redirect.php
@@ -1,10 +1,11 @@
 <?php
 $servername = $_SERVER["SERVER_NAME"];
-list($host,$servername,$tld) = explode(".",$servername)
+list($host,$servername,$tld) = explode(".",$servername);
+echo $host;
+exit;
 
-
-if ($host == "operations")  { 
-   header( "HTTP/1.1 301 Moved Permanently" ); 
+if ($host == "operations"){ 
+   header("HTTP/1.1 301 Moved Permanently" ); 
    header("Location: https://illinoisfirejuniors.sharepoint.com/sites/group.operations/SitePages/Home.aspx"); 
 }
 
