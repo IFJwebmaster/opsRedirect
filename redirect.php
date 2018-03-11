3c534a07b4cd39003206fc8c4b2700b69a19c6d6
scottHargus <scotteh@mydigitalhome.net>
2018-03-11 10:57:51 -0500

    Test

--- redirect.php
+++ redirect.php
@@ -1,7 +1,14 @@
 <?php
-$domain = $_SERVER["SERVER_NAME"];
-echo $domain;
-echo gethostbyname();
+$servername = $_SERVER["SERVER_NAME"];
+list($host,$servername,$tld) = explode(".",$servername)
 
+
+if ($host == "operations")  { 
+   header( "HTTP/1.1 301 Moved Permanently" ); 
+   header("Location: https://illinoisfirejuniors.sharepoint.com/sites/group.operations/SitePages/Home.aspx"); 
+}
+
+/* Make sure that code below does not get executed when we redirect. */
+exit;
 
 ?>
