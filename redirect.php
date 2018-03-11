349ac888f91094cff4347c410eef19324bcc99eb
scottHargus <scotteh@mydigitalhome.net>
2018-03-11 11:00:08 -0500

    push

--- redirect.php
+++ redirect.php
@@ -1,14 +1,15 @@
 <?php
 $servername = $_SERVER["SERVER_NAME"];
 list($host,$servername,$tld) = explode(".",$servername);
-echo $host;
-exit;
 
 if ($host == "operations"){ 
    header("HTTP/1.1 301 Moved Permanently" ); 
    header("Location: https://illinoisfirejuniors.sharepoint.com/sites/group.operations/SitePages/Home.aspx"); 
 }
-
+else {
+    header("HTTP/1.1 302 Temporarily Permanently" ); 
+    header("Location: http://www.illinoisfirejuniors.com"); 
+}
 /* Make sure that code below does not get executed when we redirect. */
 exit;
 
