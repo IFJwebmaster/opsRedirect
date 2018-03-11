66fe113c82d0c0534e20886082a6dad59a0491e7
scottHargus <scotteh@mydigitalhome.net>
2018-03-11 11:05:14 -0500

    Expand

--- redirect.php
+++ redirect.php
@@ -1,7 +1,7 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+//error_reporting(E_ALL);
+//ini_set('display_errors', '1');
 
 $servername = $_SERVER["SERVER_NAME"];
 list($host,$servername,$tld) = explode(".",$servername);
@@ -10,6 +10,10 @@ if ($host == "operations"){
    header("HTTP/1.1 301 Moved Permanently" ); 
    header("Location: https://illinoisfirejuniors.sharepoint.com/sites/group.operations/SitePages/Home.aspx"); 
 }
+if ($host == "ambassadors" || $host = "ambassador"){ 
+    header("HTTP/1.1 301 Moved Permanently" ); 
+    header("Location: https://illinoisfirejuniors.sharepoint.com/sites/ambassadors"); 
+ }
 else {
     header("HTTP/1.1 302 Temporarily Permanently" ); 
     header("Location: http://www.illinoisfirejuniors.com"); 
