05b0b6cc1f4e9800b1cddcd89bf32cc3e72b82b1
scottHargus <scotteh@mydigitalhome.net>
2018-03-11 11:17:17 -0500

    Updated

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <?php
-/* header( “HTTP/1.1 301 Moved Permanently” ); */
-header("Location: https://illinoisfirejuniors.sharepoint.com/sites/group.operations/SitePages/Home.aspx"); /* Redirect browser */
 
+//error_reporting(E_ALL);
+//ini_set('display_errors', '1');
+
+$servername = $_SERVER["SERVER_NAME"];
+list($host,$servername,$tld) = explode(".",$servername);
+
+if ($host == "operations"){ 
+   header("HTTP/1.1 301 Moved Permanently" ); 
+   header("Location: https://illinoisfirejuniors.sharepoint.com/sites/group.operations/SitePages/Home.aspx"); 
+}
+if ($host == "ambassadors" || $host = "ambassador"){ 
+    header("HTTP/1.1 301 Moved Permanently" ); 
+    header("Location: https://illinoisfirejuniors.sharepoint.com/sites/ambassadors"); 
+ }
+else {
+    header("HTTP/1.1 302 Temporarily Permanently" ); 
+    header("Location: http://www.illinoisfirejuniors.com"); 
+}
 /* Make sure that code below does not get executed when we redirect. */
 exit;
 
